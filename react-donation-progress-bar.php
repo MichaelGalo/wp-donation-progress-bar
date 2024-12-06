2c4d6af7534dbb8479c61c0151cb64e2c18d4575
Michael Galo <michael@michaelthinks.co>
2024-12-06 11:50:40 -0600

    initial commit

---
+++ react-donation-progress-bar.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name:     React Donation Progress Bar
+ * Plugin URI:      PLUGIN SITE HERE
+ * Description:     PLUGIN DESCRIPTION HERE
+ * Author:          YOUR NAME HERE
+ * Author URI:      YOUR SITE HERE
+ * Text Domain:     react-donation-progress-bar
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         React_Donation_Progress_Bar
+ */
+
+// Your code starts here.
